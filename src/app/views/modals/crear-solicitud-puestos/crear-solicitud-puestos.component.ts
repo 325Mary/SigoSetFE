e8a9d81f22594dfdf767a1a79e8824b4accc2bd5
325mary <mcordoba577@misena.edu.co>
2024-07-10 10:03:35 -0500

    crear  solicitar puestos

--- src/app/views/modals/crear-solicitud-puestos/crear-solicitud-puestos.component.ts
+++ src/app/views/modals/crear-solicitud-puestos/crear-solicitud-puestos.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/
 import { SedesService } from '../../../services/sedes/sedes.service';
 import { PuestosEXcentroService } from '../../../services/PuestosXcentro/puestos-excentro.service';
 import { PuestosVXcentroService } from '../../../services/PuestosXcentro/puestos-vxcentro.service';
+import { SolicitudPuestosService } from '../../../services/solicitudes/solicitud-puestos.service';
+import { formatDate } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear-solicitud-puestos',
@@ -13,16 +16,20 @@ export class CrearSolicitudPuestosComponent {
   @Output() actualizarLista = new EventEmitter<void>();
   @Input() mostrarModalSolicitarPuestos: boolean = false;
   @Output() closeModal = new EventEmitter<void>();
+
   puestoVxCentro: any[] = [];
   puestoExCentro: any[] = [];
   sedes: any[] = [];
   tipoPuestoSeleccionado: string;
-  puestosSolicitados: { puesto: string, cantidad: number }[] = [];
+  puestosDisponibles: any[] = [];
+  puestosSolicitados: { tipo: string, descripcion: string, idempresa: number, idpuesto: number, cantidad: number }[] = [];
+  nuevoPuesto: { descripcionVHumana?: string, descripcion?: string, descripcionSolicitud?: string, sede?: string } = {};
 
   constructor(
     private sedesService: SedesService,
-    private _puestosEXCentroService: PuestosEXcentroService,
-    private _puestosVXCentroService: PuestosVXcentroService,
+    private _puestosEXcentroService: PuestosEXcentroService,
+    private _puestosVXcentroService: PuestosVXcentroService,
+    private solicitudPuestosService: SolicitudPuestosService
   ) { }
 
   ngOnInit(): void { }
@@ -36,10 +43,13 @@ export class CrearSolicitudPuestosComponent {
   }
 
   obtenerPuestosVPorCentro(idcentro_formacion: number): void {
-    this._puestosVXCentroService.obtenerPuestosVxCentro(idcentro_formacion).subscribe(
+    this._puestosVXcentroService.obtenerPuestosVxCentro(idcentro_formacion).subscribe(
       (response) => {
         this.puestoVxCentro = response.data;
-        console.log('vxcentro:', response.data);
+        if (this.tipoPuestoSeleccionado === 'humana') {
+          this.filtrarPuestosDisponibles();
+        }
+        console.log(response.data);
       },
       (error) => {
         console.error(error);
@@ -48,10 +58,13 @@ export class CrearSolicitudPuestosComponent {
   }
 
   obtenerPuestosVEPorCentro(idcentro_formacion: number): void {
-    this._puestosEXCentroService.obtenerPuestosExCentro(idcentro_formacion).subscribe(
+    this._puestosEXcentroService.obtenerPuestosExCentro(idcentro_formacion).subscribe(
       (response) => {
         this.puestoExCentro = response.data;
-        console.log('vexcentro:', response.data);
+        if (this.tipoPuestoSeleccionado === 'electronica') {
+          this.filtrarPuestosDisponibles();
+        }
+        console.log(response.data);
       },
       (error) => {
         console.error(error);
@@ -63,7 +76,6 @@ export class CrearSolicitudPuestosComponent {
     this.sedesService.obtenerSedesPorCentroFormacion(idCentroFormacion).subscribe(
       (sedes) => {
         this.sedes = sedes;
-        console.log(sedes);
       },
       (error) => {
         console.error(error);
@@ -73,29 +85,92 @@ export class CrearSolicitudPuestosComponent {
 
   onTipoPuestoChange(tipoPuesto: string): void {
     this.tipoPuestoSeleccionado = tipoPuesto;
+    this.filtrarPuestosDisponibles();
   }
 
-  agregarPuesto(puesto: string, cantidad: number): void {
-    if (puesto && cantidad > 0) {
-      this.puestosSolicitados.push({ puesto, cantidad });
+  filtrarPuestosDisponibles(): void {
+    if (this.tipoPuestoSeleccionado === 'humana') {
+      this.puestosDisponibles = this.puestoVxCentro.filter(puesto =>
+        !this.puestosSolicitados.some(solicitado => solicitado.descripcion === puesto.descripcionVHumana)
+      );
+    } else if (this.tipoPuestoSeleccionado === 'electronica') {
+      this.puestosDisponibles = this.puestoExCentro.filter(puesto =>
+        !this.puestosSolicitados.some(solicitado => solicitado.descripcion === puesto.descripcion)
+      );
     }
+  }
+
+  onPuestoChange(): void {
+    if (this.tipoPuestoSeleccionado === 'humana' && this.nuevoPuesto.descripcionVHumana) {
+      const puestoSeleccionado = this.puestoVxCentro.find(puesto => puesto.descripcionVHumana === this.nuevoPuesto.descripcionVHumana);
+      if (puestoSeleccionado) {
+        this.puestosSolicitados.push({
+          tipo: 'humana',
+          descripcion: puestoSeleccionado.descripcionVHumana,
+          idpuesto: puestoSeleccionado.idpuesto,
+          idempresa: puestoSeleccionado.idempresa,
+          cantidad: 1
+        });
+      }
+    } else if (this.tipoPuestoSeleccionado === 'electronica' && this.nuevoPuesto.descripcion) {
+      const puestoSeleccionado = this.puestoExCentro.find(puesto => puesto.descripcion === this.nuevoPuesto.descripcion);
+      if (puestoSeleccionado) {
+        this.puestosSolicitados.push({
+          tipo: 'electronica',
+          descripcion: puestoSeleccionado.descripcion,
+          idpuesto: puestoSeleccionado.idpuesto,
+          idempresa: puestoSeleccionado.idempresa,
+          cantidad: 1
+        });
+      }
+    }
+    this.filtrarPuestosDisponibles();
+  }
+
+  onCantidadChange(index: number, nuevaCantidad: number): void {
+    this.puestosSolicitados[index].cantidad = nuevaCantidad;
   }
 
   eliminarPuesto(index: number): void {
     this.puestosSolicitados.splice(index, 1);
+    this.filtrarPuestosDisponibles();
   }
 
   onSubmit(): void {
-    if (this.puestosSolicitados.length > 0) {
-      const solicitudData = {
-        puestos: this.puestosSolicitados,
-        centroSeleccionado: this.centroSeleccionado,
-      };
-      console.log('Solicitud enviada:', solicitudData);
-      this.actualizarLista.emit();
-      this.closeModal.emit();
-    } else {
-      console.log('No se han agregado puestos a la solicitud');
-    }
+    const fechaSolicitud = formatDate(new Date(), 'yyyy-MM-dd HH:mm:ss', 'en');
+
+    const solicitudes = this.puestosSolicitados.map(puesto => ({
+      idcentro_formacion: this.centroSeleccionado.idcentro_formacion,
+      idempresa: puesto.idempresa,
+      idpuesto: puesto.idpuesto,
+      idsede_formacion: this.sedes.find(sede => sede.sede_formacion === this.nuevoPuesto.sede)?.idsede_formacion,
+      tipo_puesto: puesto.tipo,
+      cantidad_solicitada: puesto.cantidad,
+      descripcion_Solicitud: this.nuevoPuesto.descripcionSolicitud,
+      fecha_solicitud: fechaSolicitud
+    }));
+
+    solicitudes.forEach(solicitud => {
+      this.solicitudPuestosService.crearsolicitudes_puestosl(solicitud).subscribe(
+        (response) => {
+          console.log('Solicitud creada:', response);
+          Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'La solicitud ha sido creada exitosamente'
+          });
+          this.actualizarLista.emit();
+          this.closeModal.emit();
+        },
+        (error) => {
+          console.error('Error al crear la solicitud:', error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Hubo un error al crear la solicitud'
+          });
+        }
+      );
+    });
   }
 }
